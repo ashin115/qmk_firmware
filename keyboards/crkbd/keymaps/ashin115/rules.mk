661d9b0d83be7e2d2a60c1a14530264b893461a4
ashin115 <ashinsphilip2002@gmail.com>
2024-05-24 20:50:06 +0530

    right master

--- keyboards/crkbd/keymaps/ashin115/rules.mk
+++ keyboards/crkbd/keymaps/ashin115/rules.mk
@@ -2,7 +2,9 @@ MOUSEKEY_ENABLE = yes    # Mouse keys
 RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
 OLED_DRIVER     = ssd1306
-WPM_ENABLE      = yes
+WPM_ENABLE      = no
+OLED_DRIVER_ENABLE = yes
 LTO_ENABLE      = yes
 COMBO_ENABLE    = yes
 BOOTLOADER = rp2040
+
